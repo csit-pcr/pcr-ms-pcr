1fcad09608b82da386efbf72b8f39648a585463e
levi85 <joshua.zhangzy@gmail.com>
2018-12-01 17:34:28 +0800

    inserted **/config/application.properties to gitignore for security reasons

---
+++ src/main/java/sg/gov/dev/csit/pcrmspcr/PcrMsPcrApplication.java
@@ -0,0 +1,12 @@
+package sg.gov.dev.csit.pcrmspcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcrMsPcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcrMsPcrApplication.class, args);
+	}
+}
---
+++ src/test/java/sg/gov/dev/csit/pcrmspcr/PcrMsPcrApplicationTests.java
@@ -0,0 +1,16 @@
+package sg.gov.dev.csit.pcrmspcr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PcrMsPcrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
